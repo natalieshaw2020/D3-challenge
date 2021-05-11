b830aea274d316956cb9da9d4239d3c48b7c254c
natalieshaw2020 <nshaw@heartlandcompany.com>
2021-05-11 18:18:20 -0500

    edit

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,5 +1,5 @@
 // class activity day 3, level 9
-var svgWidth = 1000;
+var svgWidth = 900;
 var svgHeight = 700;
 var margin = {top: 20, right: 40, bottom: 60, left: 100};
 var width = svgWidth - margin.left - margin.right;
